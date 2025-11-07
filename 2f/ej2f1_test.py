e73d31078eabcd1838f8b40474902633d775d1f1
Ismael Benito Altamirano <hola@ismaelbenito.com>
2025-11-07 17:37:13 +0100

    Fix 2f

--- 2f/ej2f1_test.py
+++ 2f/ej2f1_test.py
@@ -17,9 +17,10 @@ def test_main_home_route(client):
     response = client.get("/api/v1/")
     assert response.status_code == 200, "El código de estado debe ser 200"
     assert response.data, "La respuesta debe contener datos"
-    assert b"Bienvenida" in response.data.lower() or \
-           b"Benvinguda" in response.data.lower() or \
-           b"Welcome" in response.data.lower(), \
+    response_lower = response.data.lower()
+    assert (b"bienvenida" in response_lower or
+            b"benvinguda" in response_lower or
+            b"welcome" in response_lower), \
            "La respuesta debe contener un mensaje de bienvenida"
 
 def test_main_about_route(client):
